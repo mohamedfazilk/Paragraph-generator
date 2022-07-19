1cc3c8d31a2bdf07b19a9748dec143fe80a9f8a4
mohamedfazilk <2114921@cognizant.com>
2022-07-19 22:02:25 +0530

    Adding static data in para

--- src/App.js
+++ src/App.js
@@ -23,6 +23,18 @@ function App() {
      onChange={(e)=>setCount(e.target.value)} />
       <button type='submit' className='btn'>Generator</button>
    </form>
+
+   <article className='lorem-text'>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea a perspiciatis 
+       possimus excepturi! Provident voluptatem amet officia, vel magni,
+        quo veritatis praesentium, sed quasi ipsam eius ea nostrum optio totam?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea a perspiciatis 
+       possimus excepturi! Provident voluptatem amet officia, vel magni,
+        quo veritatis praesentium, sed quasi ipsam eius ea nostrum optio totam?
+        </p>
+   </article>
   
 
  </section>
